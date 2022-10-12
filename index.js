0cfbafb63efca017476210bde5eb3ea44e3adb9a
pina7-cyber <patricia.borovski@web.de>
2022-10-12 13:37:00 +0200

    mongoDB backend

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //this is the backend of phonebook
-
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const Person = require("./models/person");
 const morgan = require("morgan");
 const cors = require("cors");
 
@@ -30,7 +31,7 @@ let persons = [
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static("build"));
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
@@ -48,44 +49,40 @@ app.get("/info", (req, res) => {
   res.send(info);
 });
 
-const generateId = (max) => {
-  return Math.floor(Math.random() * max);
-};
-
 app.post("/api/persons", morgan(":body"), (request, response) => {
   const body = request.body;
-
   if (!body.name) {
     return response.status(400).json({
       error: "name missing",
     });
-  }
-
-  if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  if (!body.number) {
+  } else if (!body.number) {
     return response.status(400).json({
       error: "number missing",
     });
+  } else {
+    Person.find({ name: body.name }).then((twin) => {
+      if (twin[0]) {
+        console.log(`name ${twin[0].name} already exists`);
+        return response.status(400).json({
+          error: "name must be unique",
+        });
+      } else {
+        const person = new Person({
+          name: body.name,
+          number: body.number || false,
+        });
+        person.save().then((savedPerson) => {
+          response.json(savedPerson);
+        });
+      }
+    });
   }
-
-  const person = {
-    name: body.name,
-    number: body.number || false,
-    id: generateId(10000),
-  };
-
-  persons = persons.concat(person);
-
-  response.json(person);
 });
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -96,17 +93,12 @@ app.delete("/api/persons/:id", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id).then((person) => {
+    response.json(person).catch((err) => response.status(404).end());
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://phonebook:${password}@phonebook.teryphu.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  date: Date,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+mongoose.connect(url).then((result) => {
+  console.log("connected");
+  if (process.argv.length == 3) {
+    Person.find({})
+      .then((result) => {
+        console.log("phonebook:");
+        result.forEach((person) => {
+          console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+      })
+      .catch((err) => console.log(err));
+  } else if (process.argv.length == 4) {
+    console.log("Please provide additional Data");
+    mongoose.connection.close();
+  } else {
+    const note = new Person({
+      name: process.argv[3],
+      number: process.argv[4],
+      date: new Date(),
+    });
+    note
+      .save()
+      .then((result) => {
+        console.log(
+          `added ${result.name} number ${result.number} to phonebook`
+        );
+        mongoose.connection.close();
+      })
+      .catch((err) => console.log(err));
+  }
+});
